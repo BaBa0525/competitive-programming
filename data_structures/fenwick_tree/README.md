b61fe4a111faf7e8f82a8fc12377cbf089fe11bb
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-04-09 22:38:24 +0800

    fenwick tree

---
+++ data_structures/fenwick_tree/README.md
@@ -0,0 +1,33 @@
+# Fenwick Tree
+
+## Usage
+
+Prefix sum or range sum with all the actions in complexity `O(logn)`.
+
+Complexity:
+
+- query `O(logn)`
+- query `O(logn)`
+
+## Comparison
+
+If no updates, we can do prefix sum by an array.
+
+```cpp
+std::vector<int> s;
+
+for (int i = 0; i < s.size(); i++) {
+  if (i == 0) continue;
+  s[i] += s[i - 1];
+}
+```
+
+Complexity:
+
+- query `O(1)`
+- update `O(n)`
+
+## Reference
+
+- [hackmd](https://hackmd.io/@wiwiho/cp-note/%2F%40wiwiho%2FCPN-binary-indexed-tree)
+- [geeksforgeeks](https://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/)
---
+++ data_structures/fenwick_tree/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+#define log_val(x)                                                     \
+  do {                                                                 \
+    std::cout << "[" << __FILE__ << ":" << __LINE__ << "(" << __func__ \
+              << ")] " << #x << " = " << x << std::endl;               \
+  } while (0)
+
+class FenwickTree {
+ public:
+  FenwickTree(const std::vector<int> &s) : tree(s.size() + 1, 0) {
+    for (int i{1}; i < s.size() + 1; i++) {
+      update(i, s[i - 1]);
+    }
+
+    for (int i{1}; i < tree.size(); i++) {
+      log_val(tree[i]);
+    }
+  }
+
+  /* lowest not zero-bit */
+  int lnzb(int n);
+  int prefix_sum(int n);
+
+  /* range sum of [s, e) */
+  int range_sum(int s, int e);
+  void update(int n, int val);
+
+ private:
+  /* binary indexed tree */
+  std::vector<int> tree;
+};
+
+int FenwickTree::lnzb(int n) { return n & (-n); }
+
+void FenwickTree::update(int n, int val) {
+  while (n < tree.size()) {
+    tree[n] += val;
+    n += lnzb(n);
+  }
+}
+
+int FenwickTree::prefix_sum(int n) {
+  int sum{0};
+
+  while (n > 0) {
+    sum += tree[n];
+
+    /* update self to the closest left ancester */
+    n = n - lnzb(n);
+  }
+
+  return sum;
+}
+
+int FenwickTree::range_sum(int s, int e) {
+  return prefix_sum(e) - prefix_sum(s);
+}
+
+int main() {
+  std::vector<int> s{10, 6, 3, 4, 2, 9, 7, 5, 11, 1, 8};
+  FenwickTree fw_tree(s);
+
+  while (true) {
+    int s, e;
+    std::cin >> s >> e;
+    log_val(fw_tree.range_sum(s, e));
+  }
+
+  return 0;
+}
